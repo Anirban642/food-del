2d64e3c14d4633bb1202b825c1ad99a18da47fdf
Anirban642 <anirbandas64237@gmail.com>
2026-09-10 17:05:51 +0530

    updated env

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://anirban64237:KmCZo0jBQUvJzOPW@cluster0.zdhdr.mongodb.net/food-del').then(()=>console.log("DB Connected")
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected")
     );
 }
-
-// mongodb+srv://anirban64237:@cluster0.zdhdr.mongodb.net/?appName=Cluster0
