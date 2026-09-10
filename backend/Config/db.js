109c481f0f879ec0668e93ca6edd0d4191446510
Anirban642 <anirbandas64237@gmail.com>
2026-09-10 16:50:46 +0530

    Updated Mongo URI

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://anirban64237:babai2003@cluster0.zdhdr.mongodb.net/food-del').then(()=>console.log("DB Connected")
+    await mongoose.connect('mongodb+srv://anirban64237:KmCZo0jBQUvJzOPW@cluster0.zdhdr.mongodb.net/food-del').then(()=>console.log("DB Connected")
     );
 }
+
+// mongodb+srv://anirban64237:@cluster0.zdhdr.mongodb.net/?appName=Cluster0
